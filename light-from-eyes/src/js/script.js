6c835ae8ec90a4579f045270440bb5a1789ae131
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2022-09-25 21:32:10 +0300

    feat: change styles

--- light-from-eyes/src/js/script.js
+++ light-from-eyes/src/js/script.js
@@ -353,4 +353,35 @@ window.onload = async () => {
   }
 
   const partnersCarousel = new PartnersCarousel();
+
+  const inputPhone = document.querySelector('.form-send-phone--input-phone');
+  const btnSubmit = document.querySelector('.form-send-phone--btn-submit');
+
+  btnSubmit.addEventListener('click', () => {
+    popupJoin.classList.remove('is-popup-show');
+    document.body.classList.remove('is-nonscrollable');
+  });
+
+  inputPhone.addEventListener('input', () => {
+    if (!inputPhone.checkValidity()) {
+      btnSubmit.setAttribute('disabled', '');
+    } else {
+      btnSubmit.removeAttribute('disabled');
+    }
+  });
+
+  const btnJoin = document.querySelector('.main-screen-left--btn-join');
+  const popupJoin = document.querySelector('.popup-join');
+
+  btnJoin.addEventListener('click', () => {
+    popupJoin.classList.add('is-popup-show');
+    document.body.classList.add('is-nonscrollable');
+  });
+
+  popupJoin.addEventListener('click', (e) => {
+    if (e.target.classList.contains('is-popup-show')) {
+      popupJoin.classList.remove('is-popup-show');
+      document.body.classList.remove('is-nonscrollable');
+    }
+  });
 };
